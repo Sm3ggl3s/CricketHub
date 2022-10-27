42831460efb753a48d5ad74c7637c733e173e5d3
WesTheHuman2000 <jlin22@uncc.edu>
2022-10-27 13:55:51 -0400

    worked on homepage, stil WIP, going to talk to you guys, see what you think

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
